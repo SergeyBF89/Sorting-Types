1b7de10869f853627bd6c9beb0cd13e174c80f4e
Sergey <sergnikolaev89@yandex.ru>
2023-01-13 21:23:30 +0300

    Добавлены два вспомогательных модуля и организация структуры проекта

---
+++ dotnet/SelectionSort/Infastructure.cs
@@ -0,0 +1,14 @@
+using static System.Console;
+public static class Infastructure
+{
+    public static int[] CreateArray(int size, int min = 0, int max = 10)
+    {
+        return Enumerable.Range(1, size)
+                .Select(item => Random.Shared.Next(min, max))
+                .ToArray();
+    }
+    public static void Show(int[] array)
+    {
+        WriteLine($"[{string.Join(',', array)}]");
+    }
+}
